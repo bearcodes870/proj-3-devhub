666b1e41c2cca15a762cb4250f377f9b3c0e3902
Bear <bear.ranasinghe@gmail.com>
2021-01-09 17:30:34 -0500

    renders template

--- main_app/views.py
+++ main_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>similar, but different to existing social media</h1>')
+    return render(request, 'home.html')
