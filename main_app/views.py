e4c993b60c75701f79c17ea11622a896bf981c4d
Bear <bear.ranasinghe@gmail.com>
2021-01-09 22:30:54 -0500

    homepage renders user index renders

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+class User:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name):
+    self.name = name
+
+users = [
+  User('Lolo'),
+  User('Sachi'),
+  User('Raven')
+]
+
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def users_index(request):
+    return render(request, 'users/index.html', { 'users': users })
