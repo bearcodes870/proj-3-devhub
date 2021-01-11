df53ad4be1eb46fa722b8d539a5705103d4a224d
Matt <matt-paul@live.com>
2021-01-10 19:43:52 -0500

    added the developer model, the developer index page, and the nav bar

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('developers/', views.developers_index, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Developer
 
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def developers_index(request):
+    developers = Developer.objects.all()
+    return render(request, 'developers/index.html', { 'developers': developers })
