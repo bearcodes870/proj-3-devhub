4453c9483d3868646b7028dcfb272f524699356b
Bear <bear.ranasinghe@gmail.com>
2021-01-13 20:16:27 -0500

    add project form works

--- main_app/views.py
+++ main_app/views.py
@@ -54,6 +54,10 @@ def projects_index(request):
     projects = Project.objects.all()
     return render(request, 'projects/index.html', { 'projects': projects })
 
+def projects_detail(request, project_id):
+    project = Project.objects.get(id=project_id)
+    return render(request, 'home.html', { 'project': project })
+
 
 def signup(request):
   error_message = ''
